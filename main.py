164d67b547075324435a0d2d4db18f8d55c5d04a
JIMMY CHO <bberryw31@gmail.com>
2025-08-25 22:01:35 -0700

    Create game class

--- main.py
+++ main.py
@@ -1,40 +1,39 @@
-from settings import *
-from pytmx.util_pygame import load_pygame
+from settings import *  # Import all our constants
 
 
 class Game:
     def __init__(self):
         # initialize pygame
         pygame.init()
-        self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-        pygame.display.set_caption("ReRollRPG 2")
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption(TITLE)
         self.clock = pygame.time.Clock()
         self.running = True
 
-        # setup
-        self.setup()
-
-    def setup(self):
-        # load map
-        game_map = load_pygame('../data/maps/lv1.tmx')
-        print(game_map)
-
     def run(self):
+        # game loop
         while self.running:
-            # dt
-            dt = self.clock.tick() / 1000
+            self.events()
+            self.update()
+            self.draw()
 
-            # event loop
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running = False
+    def events(self):
+        # event loop
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
 
-        pygame.quit()
+    def update(self):
+        # update game
+        self.clock.tick(FPS)
+
+    def draw(self):
+        # draw game
+        self.screen.fill(BLACK)
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
-    # run game
     game = Game()
     game.run()
-
-
+    pygame.quit()
--- settings.py
+++ settings.py
@@ -1,4 +1,16 @@
 import pygame
+# Game settings
 
+# Window
+TITLE = "ReRollRPG 2"
+TILE_SIZE = 32
+ORIGINAL_TILES_IZE = 16
+WIDTH = 20 * TILE_SIZE
+HEIGHT = 15 * TILE_SIZE
+FPS = 60
 
-WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
