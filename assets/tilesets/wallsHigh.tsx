2235bad3c4d77e1c733c04658a9e50b3a22af7ea
JIMMY CHO <bberryw31@gmail.com>
2025-08-28 02:07:04 -0700

    Refactor tileset directory

--- assets/tilesets/wallsHigh.tsx
+++ assets/tilesets/wallsHigh.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="wallsHigh" tilewidth="16" tileheight="32" tilecount="96" columns="24">
- <image source="0x72_DungeonTilesetII_v1.7/atlas_walls_high-16x32.png" width="384" height="128"/>
+ <image source="../downloads/0x72_DungeonTilesetII_v1.7/atlas_walls_high-16x32.png" width="384" height="128"/>
 </tileset>
