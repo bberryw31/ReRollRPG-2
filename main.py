9d5080cabed0ec69939b111b7aa3caa2f7251eb8
JIMMY CHO <bberryw31@gmail.com>
2025-09-06 15:20:05 -0700

    Refactor interaction manager

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from settings import *
 from level import Level
 from sprites.player import Player
 from sprites.enemy import Enemy
-from interaction import InteractionManager, InteractionType
+from interaction_manager import InteractionManager, InteractionType
 from ui_manager import UIManager
 
 
