232a629f261a011cda39e6f5db7ead583a3cf2d9
JIMMY CHO <bberryw31@gmail.com>
2025-09-06 13:49:17 -0700

    Implement random character generation logic

--- character.py
+++ character.py
@@ -1,5 +1,9 @@
+import random
+
+
 class CharacterGenerator:
     def __init__(self):
+        # classes
         self.classes = {
             "Warrior": {
                 "name": "Warrior",
@@ -22,3 +26,37 @@ class CharacterGenerator:
                 "sprite_path": "assassin"
             }
         }
+
+    def generate_random_character(self):
+        # random stats (20-25 total points)
+        stats = self.generate_random_stats()
+
+        # random class
+        class_name = random.choice(list(self.classes.keys()))
+        character_class = self.classes[class_name]
+
+        # random HP
+        base_hp = random.randint(5, 10)
+
+        character = {
+            "class": character_class,
+            "stats": stats,
+            "HP": base_hp,
+            "max_HP": base_hp,
+            "roll_count": 10
+        }
+
+        return character
+
+    def generate_random_stats(self):
+        stats = {"str": 0, "dex": 0, "int": 0, "luc": 0}
+        stat_names = list(stats.keys())
+
+        total_points = random.randint(20, 25)
+
+        # randomly distribute points
+        for _ in range(total_points):
+            random_stat = random.choice(stat_names)
+            stats[random_stat] += 1
+
+        return stats
