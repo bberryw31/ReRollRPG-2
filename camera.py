d40dc98f4053fb87625c2f8a8561e32b542a7fc8
JIMMY CHO <bberryw31@gmail.com>
2025-09-06 23:16:21 -0700

    Remove UI offset from camera

--- camera.py
+++ camera.py
@@ -23,8 +23,8 @@ class Camera:
     def update(self, target):
         # update to follow target
         # center camera on target
-        self.target_x = target.rect.centerx - GAME_AREA_WIDTH // 2
-        self.target_y = target.rect.centery - GAME_AREA_HEIGHT // 2
+        self.target_x = target.rect.centerx - WIDTH // 2
+        self.target_y = target.rect.centery - HEIGHT // 2
 
         # smooth camera movement
         self.camera.x += (self.target_x - self.camera.x) * CAMERA_SMOOTHING
@@ -33,14 +33,14 @@ class Camera:
         # keep camera within map bounds
         self.camera.x = max(0, self.camera.x)
         self.camera.y = max(0, self.camera.y)
-        self.camera.x = min(self.width - GAME_AREA_WIDTH, self.camera.x)
-        self.camera.y = min(self.height - GAME_AREA_HEIGHT, self.camera.y)
+        self.camera.x = min(self.width - WIDTH, self.camera.x)
+        self.camera.y = min(self.height - HEIGHT, self.camera.y)
 
     def get_visible_area(self):
         # get the area currently visible by the camera
         return pygame.Rect(
             self.camera.x,
             self.camera.y,
-            GAME_AREA_WIDTH,
-            GAME_AREA_HEIGHT
+            WIDTH,
+            HEIGHT
         )
